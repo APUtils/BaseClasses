53b1e56ff709ebaf3198ac78db355ac5befa60e8
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-02-19 19:03:55 +0600

    - [TextView] Ability to set placeholder

--- BaseClasses/Classes/TextView.swift
+++ BaseClasses/Classes/TextView.swift
@@ -20,6 +20,22 @@ open class TextView: UITextView {
         }
     }
     
+    @IBInspectable open var placeholder: String? {
+        didSet {
+            if placeholder == oldValue { return }
+            updatePlaceholder()
+        }
+    }
+    
+    // ******************************* MARK: - Lazy
+    
+    open private(set) lazy var placeholderLabel: UILabel = {
+        let l = UILabel()
+        l.textColor = UIColor(red: 199/255, green: 199/255, blue: 205/255, alpha: 1)
+        
+        return l
+    }()
+    
     // ******************************* MARK: - Initialization and Setup
     
     public override init(frame: CGRect, textContainer: NSTextContainer?) {
@@ -40,6 +56,7 @@ open class TextView: UITextView {
     
     private func setup() {
         textContainer.lineFragmentPadding = 0
+        NotificationCenter.default.addObserver(self, selector: #selector(onTextChange), name: UITextView.textDidChangeNotification, object: self)
         
         configure()
     }
@@ -53,5 +70,28 @@ open class TextView: UITextView {
     private func configureInsets() {
         let insets = UIEdgeInsets(top: 0, left: textSideInset, bottom: 0, right: textSideInset)
         textContainerInset = insets
+    }
+    
+    // ******************************* MARK: - Update
+    
+    private func updatePlaceholder() {
+        addSubview(placeholderLabel)
+        
+        placeholderLabel.text = placeholder
+        if let pointSize = font?.pointSize {
+            placeholderLabel.font = .italicSystemFont(ofSize: pointSize)
+            placeholderLabel.sizeToFit()
+            placeholderLabel.frame.origin = .zero
+        }
+        
+        placeholderLabel.isHidden = !text.isEmpty
+    }
+    
+    // ******************************* MARK: - Notifications
+    
+    @objc fileprivate func onTextChange(_ sender: Any) {
+        if placeholder != nil {
+            placeholderLabel.isHidden = !text.isEmpty
+        }
     }
 }
